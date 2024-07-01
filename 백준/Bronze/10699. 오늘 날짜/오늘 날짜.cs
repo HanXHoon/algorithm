4b99425af9dd327614b228962f9c8c44d40d87d7
HanXHoon <134307215+HanXHoon@users.noreply.github.com>
2024-07-01 23:24:17 +0900

    [Bronze V] Title: 오늘 날짜, Time: 64 ms, Memory: 5540 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cs
@@ -0,0 +1,11 @@
+namespace CodeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string today = DateTime.Today.ToString("yyyy-MM-dd");
+            Console.WriteLine(today);
+        }
+    }
+}
