47a0f6375ccbc97b31a2ea535597b50d507143aa
HanXHoon <134307215+HanXHoon@users.noreply.github.com>
2024-07-08 19:03:53 +0900

    [Bronze II] Title: 나머지, Time: 60 ms, Memory: 5844 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.cs
+++ 백준/Bronze/3052. 나머지/나머지.cs
@@ -1,16 +1,5 @@
-namespace ConsoleApp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            string result = Enumerable.Range(0, 10)
-                                        .Select(_ => Convert.ToInt32(Console.ReadLine()) % 42)
-                                        .GroupBy(g => g)
-                                        .Count()
-                                        .ToString();
-
-            Console.WriteLine(result);
-        }
-    }
-}
+Console.WriteLine(Enumerable.Range(0, 10)
+    .Select(_ => Convert.ToInt32(Console.ReadLine()) % 42)
+    .GroupBy(g => g)
+    .Count()
+    .ToString());
