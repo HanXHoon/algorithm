ee9519480a1005225ca82d9372a150ecaad7072d
HanXHoon <134307215+HanXHoon@users.noreply.github.com>
2024-07-08 19:08:11 +0900

    Update 나머지.cs

--- 백준/Bronze/3052. 나머지/나머지.cs
+++ 백준/Bronze/3052. 나머지/나머지.cs
@@ -1,5 +1,16 @@
-Console.WriteLine(Enumerable.Range(0, 10)
-    .Select(_ => Convert.ToInt32(Console.ReadLine()) % 42)
-    .GroupBy(g => g)
-    .Count()
-    .ToString());
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = Enumerable.Range(0, 10)
+                                        .Select(_ => Convert.ToInt32(Console.ReadLine()) % 42)
+                                        .GroupBy(g => g)
+                                        .Count()
+                                        .ToString();
+
+            Console.WriteLine(result);
+        }
+    }
+}
