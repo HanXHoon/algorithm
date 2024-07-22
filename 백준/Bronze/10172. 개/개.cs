931e3e6b294498a31b13c9605f564133e6442004
HanXHoon <134307215+HanXHoon@users.noreply.github.com>
2024-07-22 19:46:05 +0900

    [Bronze V] Title: 개, Time: 60 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,15 @@
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dogStr = {"|\\_/|",
+                                "|q p|   /}",
+                                "( 0 )\"\"\"\\",
+                                "|\"^\"`    |",
+                                "||_/=\\\\__|"};
+            Console.Write(string.Join("\n", dogStr));
+        }
+    }
+}
