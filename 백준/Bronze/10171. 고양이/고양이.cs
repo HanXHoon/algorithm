34117394d3d3fbb0b84f6bbcf7c426e468e02b35
HanXHoon <134307215+HanXHoon@users.noreply.github.com>
2024-07-22 19:47:32 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,16 @@
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] catStr = {
+                                "\\    /\\",
+                                " )  ( ')",
+                                "(  /  )",
+                                " \\(__)|"                
+                                };
+            Console.Write(string.Join("\n", catStr));
+        }
+    }
+}
